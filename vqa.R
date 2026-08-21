0ece3149cf9c85bdc40757111ff56040fb210362
Brad Boyle <bboyle@email.arizona.edu>
2026-08-20 20:42:15 -0700

    vqa.R: improve error handling

--- vqa.R
+++ vqa.R
@@ -1,44 +1,68 @@
-#!/usr/bin/env Rscript
-
 # =================================================================================
 # Script:         vqa.R
-# Purpose:        Controller script for all Vegetation Quality Analysis operations.
+# Purpose:        Controller script for all Vegetation Quality Analysis operations
 # Author:         Brad Boyle (ojalaquellueva@gmail.com)
 # Date Created:   2026-08-20
 # R Version:      R version 4.4.1 (2024-06-14)
 # =================================================================================
 
-# Load CLI Parsing Library
+# ==============================================
+# Note: This script take command line arguments, 
+# and MUST be run from the shell, or the Terminal 
+# window in RStudio
+# ==============================================
+
+# ===============================================
+# Load libraries for just this script (others
+# loaded later, depending on which part of the
+# pipeline is called)
+# ===============================================
+
+# CLI Parsing Library
 if (!requireNamespace("optparse", quietly = TRUE)) {
   stop("The 'optparse' package is required. Install it using install.packages('optparse')", call. = FALSE)
 }
 library(optparse)
 
+# ===============================================
+# Parameters
+# ===============================================
+
+# ===============================================
+# Parameters
+# ===============================================
+
+# Each mode is a different step in the vqa pipeline.
+# It is also the name of the file launched ("sourced")
+# by this script, minus the ".R" extension
+allowed_modes <- c("import", "vqa.batch", "qh.net")
+
+# Help hint appended to all error messages
+hint_help <- "(Use option '-h' or '--help' for full usage details)"
+
+# ===============================================
+# Main
+# ===============================================
+
 # Define Command-Line Options
 option_list <- list(
   make_option(
-    c("-m", "--mode"), 
-    type = "character", 
-    default = NULL,
+    c("-m", "--mode"), type = "character", default = NULL,
     help = "Execution mode: 'import', 'vqa_batch', or 'qh.net' [REQUIRED]", 
-    metavar = "MODE"
-    ),
-  
+    metavar = "MODE"),
   make_option(c("-p", "--project"), type = "character", default = NULL, 
     help = "Project code [REQUIRED]", metavar = "PROJ"),
-  
   make_option(c("-a", "--assess"), type = "character", default = NULL, 
     help = "Assessment code [REQUIRED]", metavar = "ASSESS")
-  
 )
 
 # Parse Arguments
 parser <- OptionParser(
   usage = "usage: %prog --mode <MODE> --project <PROJ> --assess <ASSESS>",
+  add_help_option = TRUE, 
   option_list = option_list,
   description = "\nVegetation Quality Assessment analysis pipeline."
 )
-
 args <- parse_args(parser)
 
 # ==================
@@ -46,37 +70,45 @@ args <- parse_args(parser)
 # ==================
 
 # Enforce required --mode argument
-if (is.null(args$mode)) {
-  print_help(parser)
-  stop("\nError: Missing required argument: --mode / -m", call. = FALSE)
+if (is.null(args$mode)) { # Argument missing
+  msg_err <- "Missing required option --mode | -m"
+  stop(paste0(msg_err, " ", hint_help), call. = FALSE)
 }
-allowed_modes <- c("import", "vqa_batch", "qh.net")
-if (!(args$mode %in% allowed_modes)) {
-  print_help(parser)
-  stop(paste0("\nError: Invalid mode '", args$mode, "'. Must be one of: ", 
-    paste(allowed_modes, collapse = ", ")), call. = FALSE)
+if (!(args$mode %in% allowed_modes)) { # Invalid argument
+  msg_err <- "Invalid argument for --mode | -m"
+  stop(paste0(msg_err, " ", hint_help), call. = FALSE)
 }
-
 # Assign validated option to a clean variable name
-mode <- args$mode
+opt_mode <- args$mode
 
 # Check that remaining options have values
-if (is.null(args$project)) {
-  print_help(parser)
-  stop("\nError: Missing required argument: --project / -m", call. = FALSE)
+if (is.null(args$project)) { # Argument missing
+  msg_err <- "Missing required argument: --project / -m"
+  stop(paste0(msg_err, " ", hint_help), call. = FALSE)
 }
-project <- args$project
-if (is.null(args$assess)) {
-  print_help(parser)
-  stop("\nError: Missing required argument: --assess / -m", call. = FALSE)
+# 'opt_' prefix marks this as command line setting:
+# takes precedence over parameters set in params files
+opt_project <- args$project
+
+if (is.null(args$assess)) { # Argument missing
+  msg_err <- "Missing required argument: --assess / -m"
+  stop(paste0(msg_err, " ", hint_help), call. = FALSE)
 }
-assess <- args$assess
+# 'opt_' prefix marks this as command line setting:
+# takes precedence over parameters set in params files
+opt_assess <- args$assess
 
-cat("Arguments successfully processed:\n")
-cat("  mode: ", mode, "\n", sep="")
-cat("  project: ", project, "\n", sep="")
-cat("  assess: ", assess, "\n", sep="")
+# Find where this script is located
+# Only works from shell, but OK for this script.
+initial_options <- commandArgs(trailingOnly = FALSE)
+file_arg <- "--file="
+script_name <- sub(file_arg, "", initial_options[grep(file_arg, initial_options)])
+script_dir <- dirname(script_name)
+script_dir <- normalizePath(script_dir, mustWork = TRUE)
 
-cat("\n\nOperation completed\n")
+# Launch the target script
+source(file.path(script_dir, paste0(opt_mode, ".R")))
+
+
 
 
